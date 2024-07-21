e5c590d96a44bc4572f8e43d3981579777ad7156
Ben Hilger <bhilger78@gmail.com>
2024-07-21 12:45:35 -0400

    add through maps on gobyexample

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Ben-Hilger/gobyexample
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("---- HELLO WORLD ----")
+	helloWorld()
+	fmt.Println("---- VALUES ----")
+	values()
+	fmt.Println("---- VARIABLES ----")
+	variables()
+	fmt.Println("---- CONSTANTS ----")
+	constants()
+	fmt.Println("---- FOR LOOPS ----")
+	forLoop()
+	fmt.Println("---- IF ELSE ----")
+	ifelse()
+	fmt.Println("---- SWITCH ----")
+	switchCase()
+	fmt.Println("---- ARRAYS ----")
+	arrays()
+	fmt.Println("---- SLICES ----")
+	slice()
+	fmt.Println("---- MAPS ----")
+	mapType()
+}
